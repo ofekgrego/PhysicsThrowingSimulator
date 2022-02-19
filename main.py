46eef231b7f788cc67c49045b85248fe45175fa6
Ofek Grego <Ofek Grego>
2022-02-19 17:28:58 +0200

    Make The View

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import math
+import random
+from tkinter import *
+import tkinter
 
+
+#CONST
 GRAVITY = -9.8
+CANVAS_WIDTH = 1000  #x
+CANVAS_HEIGHT = 1000 #y
+
+#FUNCTIONS
+
+def Run():
+    print("hi")
+
 
 def findDegree(V0_X, V0_Y):
     if (V0_X == 0):
@@ -32,3 +45,43 @@ def timeY0(Y0, V0_Y):
     T0 = (-V0_Y+math.sqrt(V0_Y*V0_Y-2*GRAVITY*Y0))/GRAVITY
     T1 = (-V0_Y-math.sqrt(V0_Y*V0_Y-2*GRAVITY*Y0))/GRAVITY
     return max(T0, T1)
+
+
+window = Tk()
+window.title("Corona Simulator")
+window.resizable(False,False) #disable resize
+fCan = Frame(window, width=CANVAS_WIDTH, height=CANVAS_WIDTH+260)
+fCan.pack(side=tkinter.LEFT)
+w = Canvas(fCan, width=CANVAS_WIDTH,height=CANVAS_HEIGHT, background="gray")
+w.pack()
+
+graphCanvas = Canvas(fCan,width=CANVAS_WIDTH,height=260)
+graphCanvas.pack()
+
+graphCanvas.create_text(70,30, text="X", font="helvetica 20")
+graphCanvas.create_line(50,50,50,250, width=3)
+graphCanvas.create_line(50,250,500,250, width=3)
+graphCanvas.create_text(580,30, text="Y", font="helvetica 20")
+graphCanvas.create_line(550,50,550,250, width=3)
+graphCanvas.create_line(550,250,1000,250, width=3)
+
+fInfo = Frame(window, width=200, height=CANVAS_HEIGHT)
+fInfo.pack()
+
+creditLabel = Label(fInfo, text="Made By Ofek Grego").pack()
+spaceLabel = Label(fInfo,text="").pack()
+
+explainLabel = Label(fInfo, text="Enter Velocity and Degree \nOR\n Velocity X and Velocity Y \n(Leave the other with '?')").pack()
+
+velocityLabel = Label(text="\n\nVelocity:").pack(side=tkinter.TOP)
+velocityEntry = Entry(width=5).pack(side=tkinter.TOP)
+degreeLabel = Label(text="Degree:").pack(side=tkinter.TOP)
+degreeEntry = Entry(width=5).pack(side=tkinter.TOP)
+velocityYLabel = Label(text="\n\nVelocity Y:").pack(side=tkinter.TOP)
+velocityYEntry = Entry(width=5).pack(side=tkinter.TOP)
+velocityXLabel = Label(text="Velocity X:").pack(side=tkinter.TOP)
+velocityXEntry = Entry(width=5).pack(side=tkinter.TOP)
+
+nextButton = Button(fInfo,text="Run", command=Run).pack(side=tkinter.TOP)
+
+window.mainloop()
